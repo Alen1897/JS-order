dcec13c1fcb037e388159bc5dd951ec2b6622750
Alen1897 <alen1897@hotmail.com>
2023-02-21 21:26:27 +0100

    js-lottery

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ import { politicians, folk } from "./data/data.js";
 const lottery = new Lottery(politicians);
 
 lottery.startDrawing();
+
+lottery
+  .startDrawing()
+  .then((result) => {
+    console.log("ovo je uspjesšno izlačenje, resolve");
+    console.log(result);
+  })
+  .catch((result) => {})
+  .finally(() => {});
