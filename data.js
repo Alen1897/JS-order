f473dfea1d701b88dadab2f70734806283b031d0
Alen1897 <alen1897@hotmail.com>
2023-02-23 21:26:02 +0100

    parcijalni js

---
+++ data.js
@@ -0,0 +1,24 @@
+"use strict";
+
+export const lecture = [
+  {
+    lectureName: "Html",
+    lectureLength: "40 min",
+  },
+  {
+    lectureName: "CSS",
+    lectureLength: "50 min",
+  },
+  {
+    lectureName: "JavaScript",
+    lectureLength: "60 min",
+  },
+  {
+    lectureName: "React",
+    lectureLength: "70 min",
+  },
+  {
+    lectureName: "Python",
+    lectureLength: "90 min",
+  },
+];
